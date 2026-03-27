b6fa72dab748649ebdc439cd93642fea95ec6c77
mersidd00 <mersidd00@gmail.com>
2026-03-27 23:40:04 +0100

    script.js

--- tvbox/script.js
+++ tvbox/script.js
@@ -111,7 +111,7 @@
         const formData = new FormData(form);
         const data = Object.fromEntries(formData.entries());
 
-        fetch("https://ramadan2.app.n8n.cloud/webhook/order", {
+        fetch("https://tvbox.app.n8n.cloud/webhook/order", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
