c308281d66ed7e77ed7b1008005ff2c55218d574
mersidd00 <mersidd00@gmail.com>
2026-03-28 00:06:07 +0100

    Update script.js

--- h/script.js
+++ h/script.js
@@ -107,7 +107,7 @@
         const formData = new FormData(form);
         const data = Object.fromEntries(formData.entries());
 
-        fetch("https://admin2323.app.n8n.cloud/webhook-test/order", {
+        fetch("https://tvbox.app.n8n.cloud/webhook/order", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
