c30950c905bebcd22daf48b7f9c3dfe478186c6c
mersidd00 <mersidd00@gmail.com>
2026-05-07 23:03:32 +0200

    script.js

--- pack-cuisine/script.js
+++ pack-cuisine/script.js
@@ -99,7 +99,7 @@
         const formData = new FormData(form);
         const data = Object.fromEntries(formData.entries());
 
-        fetch("https://eidlkbir.app.n8n.cloud/webhook/order", {
+        fetch("https://eideid.app.n8n.cloud/webhook/order", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
