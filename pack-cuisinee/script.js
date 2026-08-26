9da16ddac031d6eaafacf0e5986d7a31531573a1
mersidd00 <mersidd00@gmail.com>
2026-08-27 01:42:12 +0200

    Update script.js

--- pack-cuisinee/script.js
+++ pack-cuisinee/script.js
@@ -107,7 +107,7 @@
         const formData = new FormData(form);
         const data = Object.fromEntries(formData.entries());
 
-        fetch("https://sidd1232.app.n8n.cloud/webhook/order", {
+        fetch("https://sidsidd.app.n8n.cloud/webhook/order", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
